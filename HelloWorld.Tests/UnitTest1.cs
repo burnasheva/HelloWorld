4e34b9a4da636fcb489dfe6ca127782c41caad17
Nikolay Pianikov <NikolayP@live.com>
2015-05-06 18:39:53 +0300

    Initial commit

---
+++ HelloWorld.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
