ff14ebcf6f90551a2c8b29598e26652fc6f9b3c9
Nikolay Pianikov <NikolayP@live.com>
2015-05-25 16:04:40 +0300

    SetUp exception

---
+++ HelloWorld.Tests/UnitTest2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace HelloWorld.Tests
+{
+    public class UnitTest2
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            throw new Exception("SetUp exception");
+        }
+
+        [Test]
+        public void TestMethodSetUpFailed()
+        {
+        }
+    }
+}
