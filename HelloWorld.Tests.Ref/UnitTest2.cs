8612d3a7fc960b1f7271cab30d051515edd3b9cf
Nikolay Pianikov <NikolayP@live.com>
2015-05-25 16:14:06 +0300

    add tests

---
+++ HelloWorld.Tests.Ref/UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+
+using NUnit.Framework;
+
+namespace HelloWorld.Tests
+{
+    public class UnitTest2
+    {
+        private SomeClass _class;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _class = new SomeClass();
+        }
+
+        [Test, Category("SE")]
+        public void TestMethodSetUpFailed()
+        {
+        }
+    }
+}
