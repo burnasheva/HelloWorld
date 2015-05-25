a379346fbb90b42a2ea030d83fd85b2284574367
Nikolay Pianikov <NikolayP@live.com>
2015-05-25 16:05:01 +0300

    SetUp

--- HelloWorld.Tests/UnitTest2.cs
+++ HelloWorld.Tests/UnitTest2.cs
@@ -12,7 +12,7 @@ namespace HelloWorld.Tests
             throw new Exception("SetUp exception");
         }
 
-        [Test]
+        [Test, Category("SE")]
         public void TestMethodSetUpFailed()
         {
         }
