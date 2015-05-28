b81040fc9a8ee09a1f38724631f8d80d15e6d8e3
Nikolay Pianikov <NikolayP@live.com>
2015-05-28 12:31:51 +0300

    aa

---
+++ HelloWorld.Tests/UnitTest3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+using NUnit.Framework;
+
+namespace HelloWorld.Tests
+{
+    [TestFixture]
+    public class UnitTest3
+    {
+        [Test, Category("OOM")]
+        public void TestMethodOOM()
+        {
+            MakeOutOfMemory();
+        }
+
+        [Test, Category("OOMT")]
+        public void TestMethodOOMInThread()
+        {
+            var thread = new Thread(MakeOutOfMemory) { IsBackground = false };
+            thread.Start();
+            thread.Join();
+        }
+
+        private static void MakeOutOfMemory()
+        {
+            var list = new List<byte[]>();
+            while (true)
+            {
+                list.Add(new byte[0x40000000]);
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
