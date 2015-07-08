3e6c01a6779d3d11e5be895b061db8f633615798
Nikolay Pianikov <NikolayP@live.com>
2015-07-08 15:32:42 +0300

    aa

--- HellowWorld.Tests2/UnitTest1.cs
+++ HellowWorld.Tests2/UnitTest1.cs
@@ -8,8 +8,6 @@ namespace HelloWorld.Tests
     [Parallelizable]
     public class UnitTest1
     {
-        private static readonly Random Rnd = new Random();
-
         [Test]
         [Parallelizable]
         public void TestDo()
