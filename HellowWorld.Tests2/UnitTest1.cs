0239ca2118bd68065e853e6df51008a29138cb30
Nikolay Pianikov <NikolayP@live.com>
2015-07-08 15:32:35 +0300

    aaa

---
+++ HellowWorld.Tests2/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace HelloWorld.Tests
+{
+    [TestFixture]
+    [Parallelizable]
+    public class UnitTest1
+    {
+        private static readonly Random Rnd = new Random();
+
+        [Test]
+        [Parallelizable]
+        public void TestDo()
+        {
+            var a = new Class1();
+            a.Do();
+        }
+    }
+}
