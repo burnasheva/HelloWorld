1f227c3fb1d13b4fe8b685019a547f278793dcf7
Nikolay Pianikov <NikolayP@live.com>
2015-08-03 19:30:48 +0300

    Revert "aaa"
    This reverts commit 0a42a5542bbb957ed832ea608faa55c0d3664169.

--- HelloWorld.Tests/UnitTest1.cs
+++ HelloWorld.Tests/UnitTest1.cs
@@ -5,8 +5,6 @@ using NUnit.Framework;
 
 namespace HelloWorld.Tests
 {
-    using JetBrains.dotMemoryUnit;
-
     [TestFixture]
     [Parallelizable]
     public class UnitTest1
@@ -15,16 +13,10 @@ namespace HelloWorld.Tests
 
         [Test]
         [Parallelizable]
-        [AssertTraffic(AllocatedSizeInBytes = 1000)]
         public void TestMethodSuccess()
         {
-            dotMemory.Check(
-                memory =>
-                    {
-                        var a = new Class1();
-                        a.Do();
-                        Assert.AreEqual(10, memory.ObjectsCount);
-                    });
+            var a = new Class1();
+            a.Do();
         }
 
         [Test]
