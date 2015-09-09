4765c81cc8aabee7c91f23798b19c65ce2cdf856
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2015-09-09 13:15:34 +0300

    aa

--- HelloWorld.Tests.NUnit2/UnitTest2.cs
+++ HelloWorld.Tests.NUnit2/UnitTest2.cs
@@ -2,12 +2,14 @@
 
 namespace HelloWorld.Tests.NUnit2
 {
+    using System.Diagnostics.CodeAnalysis;
+
     using NUnit.Framework;
 
     [TestFixture]
     public class UnitTest2
     {
-        [SetUp]
+        [SetUp, ExcludeFromCodeCoverage]
         public void TestSetUp()
         {
             throw new StackOverflowException("Init exception");
