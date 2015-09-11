f23c4667c8a13559260385d056d1a7987e7cf317
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2015-09-11 10:12:41 +0300

    aa

--- HelloWorld/Class1.cs
+++ HelloWorld/Class1.cs
@@ -4,8 +4,11 @@ namespace HelloWorld
 {
     public class Class1
     {
+        private string str1 = null;
+
         public void Do()
         {
+            Console.WriteLine(str1?.Length ?? 10);
             Console.WriteLine("!!!");
         }
     }
