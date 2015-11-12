2d7d243ea5f6e7ced33f5d755d091ae97e88a137
Nikolay Pianikov <Nikolay.Pianikov@jetbrains.com>
2015-11-12 13:08:19 +0300

    4.5

---
+++ HelloWorld.Tests/UnitTestMef.cs
@@ -0,0 +1,38 @@
+﻿namespace HelloWorld.Tests
+{
+    using NUnit.Framework;
+    using System.ComponentModel.Composition;
+    using System.ComponentModel.Composition.Hosting;
+
+    [TestFixture]
+    public class UnitTestMef
+    {
+        public interface IClass<T>
+        {
+            string Execute();
+        }
+
+        [Export(typeof(IClass<>))]
+        public class ImplementedClass<T> : IClass<T>
+        {
+            public string Execute()
+            {
+                return "Class was imported and executed successfully";
+            }
+        }
+
+        [TestFixture]
+        public class ClassSpecs
+        {
+            [Test]
+            public void ClassShouldBeImported()
+            {
+                var catalog = new AssemblyCatalog(typeof(ImplementedClass<string>).Assembly);
+                var container = new CompositionContainer(catalog);
+                var importedInstance = container.GetExportedValue<IClass<string>>();
+
+                Assert.That(importedInstance.Execute(), Is.EqualTo("Class was imported and executed successfully"));
+            }
+        }
+    }
+}
